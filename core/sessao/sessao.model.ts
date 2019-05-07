1b7a5a4a03a10de0e05c73cbb88d0018da60d490
jsilveira2 <33058420+jsilveira2@users.noreply.github.com>
2019-05-07 09:57:27 -0300

    Delete sessao.model.ts

--- core/sessao/sessao.model.ts
+++
@@ -1,8 +0,0 @@
-import { AdmiUsuario } from '../../administration/admi-usuario/admi-usuario.model';
-
-export interface Sessao {
-    usuario: AdmiUsuario;
-    token: string;
-    expires: number;
-    conta: number;
-}
